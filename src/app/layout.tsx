adcdc33347a3a45f18e0a8a7dbbb784e33990c73
root <root@host-71405.indovm.com>
2026-03-23 21:23:29 +0000

    Fix CSS import order - move Google Fonts to HTML head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,9 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" href="/icons/icon-180x180.png" />
         <link rel="icon" href="/icons/icon-32x32.png" sizes="32x32" />
       </head>
